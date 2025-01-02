33e6fb255767b8f60e10a1102ac2e2d4bc70ed84
danwiic <danpirante9@gmail.com>
2025-01-02 17:35:52 +0800

    s

--- RR/AddRecipe.vb
+++ RR/AddRecipe.vb
@@ -2,6 +2,7 @@
     Dim userRole As String = LoginForm.currentUserRole
     Private Sub AddRecipe_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         shadowForm.SetShadowForm(Me)
+        CheckUserRole()
     End Sub
 
     Private Sub CheckUserRole()
@@ -13,5 +14,9 @@
     End Sub
     Private Sub btnAddNewRecipe_Click(sender As Object, e As EventArgs) Handles btnAddNewRecipe.Click
 
+    End Sub
+
+    Private Sub Guna2Button1_Click(sender As Object, e As EventArgs) Handles Guna2Button1.Click
+        Me.Hide()
     End Sub
 End Class
